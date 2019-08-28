8601694fe2b0a262dda510d116cf82e303a0201b
nwg-piotr <nwg.piotr@gmail.com>
2019-08-28 12:51:49 +0200

    rewritten for i3ipc 2.0.1

--- wsdnames-i3ipc-2.0.1.py
+++ wsdnames-i3ipc-2.0.1.py
@@ -3,17 +3,14 @@
 
 """
 Dynamic workspace names for Sway
-THIS VERSION DOES NOT WORK ON SWAY WITH NOT-YET-FIXED i3ipc v1.7.1 MODULE
-
-The script should also work on i3, but for some reason it works well for me on 2 machines, and loops on the 3rd one.
-Use on i3 at your own risk.
+THIS VERSION REQUIRES i3ipc >= 2.0.1
 
 Author: Piotr Miller
 e-mail: nwg.piotr@gmail.com
 Project: https://github.com/nwg-piotr/swayinfo
 License: GPL3
 
-Depends on: i3ipc-python (python-i3ipc) >1.7.1
+Depends on: python-i3ipc>=2.0.1 (i3ipc-python), python-xlib
 """
 
 from i3ipc import Connection, Event
@@ -41,55 +38,49 @@ def glyph(ws_number):
 
 # Name the workspace after the focused window name
 def assign_generic_name(i3, e):
-    try:
-        con = i3.get_tree().find_focused()
-        if not con.type == 'workspace':         # avoid renaming new empty workspaces on 'binding' event
-            # con.type == 'floating_con'        - indicates floating enabled in Sway
-            # con.floating                      - may be equal 'auto_on' or 'user_on' in i3
-            is_floating = con.type == 'floating_con' or con.floating and '_on' in con.floating
+    if not e.change == 'rename':                        # avoid looping in i3
+        try:
+            con = i3.get_tree().find_focused()
+            print('>>> ', con.type, e.change)
+            if not con.type == 'workspace':             # avoid renaming new empty workspaces on 'binding' event
+                if not e.change == 'new':
+                    # con.type == 'floating_con'        - indicates floating enabled in Sway
+                    # con.floating                      - may be equal 'auto_on' or 'user_on' in i3
+                    is_floating = con.type == 'floating_con' or con.floating and '_on' in con.floating
 
-            # Tiling mode or floating indication
-            layout = con.parent.layout
-            if layout == 'splith':
-                split_text = '⇢' if not is_floating else ''
-            elif layout == 'splitv':
-                split_text = '⇣' if not is_floating else ''
+                    # Tiling mode or floating indication
+                    layout = con.parent.layout
+                    if layout == 'splith':
+                        split_text = '⇢' if not is_floating else ''
+                    elif layout == 'splitv':
+                        split_text = '⇣' if not is_floating else ''
+                    else:
+                        split_text = ''
+
+                    ws_old_name = con.workspace().name
+                    ws_name = "%s: %s\u00a0%s %s " % (con.workspace().num, glyph(con.workspace().num), split_text, con.name)
+                    name = ws_name if len(ws_name) <= max_width else ws_name[:max_width - 1] + "…"
+
+                    i3.command('rename workspace "%s" to %s' % (ws_old_name, name))
+                else:
+                    # In sway we may open a new window w/o moving focus; let's give the workspace a name anyway.
+                    con = i3.get_tree().find_by_id(e.container.id)
+                    ws_num = con.workspace().num
+                    w_name = con.name if con.name else ''
+
+                    if w_name and ws_num:
+                        name = "%s: %s\u00a0%s" % (ws_num, glyph(ws_num), w_name)
+                        i3.command('rename workspace "%s" to %s' % (ws_num, name))
+
             else:
-                split_text = ''
+                # Give the workspace a generic name: "number: glyph" (like "1: ")
+                ws_num = con.workspace().num
+                ws_new_name = "%s: %s" % (ws_num, glyph(ws_num))
 
-            ws_old_name = con.workspace().name
-            ws_name = "%s: %s\u00a0%s %s " % (con.workspace().num, glyph(con.workspace().num), split_text, con.name)
-            name = ws_name if len(ws_name) <= max_width else ws_name[:max_width - 1] + "…"
+                i3.command('rename workspace to "{}"'.format(ws_new_name))
 
-            i3.command('rename workspace "%s" to %s' % (ws_old_name, name))
-
-        else:
-            # Give the workspace a generic name: "number: glyph" (like "1: ")
-            ws_num = con.workspace().num
-            ws_new_name = "%s: %s" % (ws_num, glyph(ws_num))
-
-            i3.command('rename workspace to "{}"'.format(ws_new_name))
-
-        print('{} focused'.format(con.type))
-
-    except Exception as ex:
-        exit(ex)
-
-
-# In sway it's possible to open a new window w/o moving focus; let's give the workspace a name anyway.
-def on_window_new(i3, e):
-    print('on_window_new OK')
-    try:
-        con = i3.get_tree().find_by_id(e.container.id)
-        ws_num = con.workspace().num
-        w_name = con.name if con.name else ''
-        print('ws_num {}, w_name {}'.format(ws_num, w_name))
-
-        if w_name and ws_num:
-            name = "%s: %s\u00a0%s" % (ws_num, glyph(ws_num), w_name)
-            i3.command('rename workspace "%s" to %s' % (ws_num, name))
-    except:
-        pass
+        except Exception as ex:
+            exit(ex)
 
 
 def main():
@@ -98,8 +89,8 @@ def main():
     i3.on(Event.WINDOW_FOCUS, assign_generic_name)
     i3.on(Event.WINDOW_TITLE, assign_generic_name)
     i3.on(Event.WINDOW_CLOSE, assign_generic_name)
-    i3.on(Event.WINDOW_NEW, on_window_new)
-    i3.on("binding", assign_generic_name)
+    i3.on(Event.WINDOW_NEW, assign_generic_name)
+    i3.on(Event.BINDING, assign_generic_name)
 
     i3.main()
 
