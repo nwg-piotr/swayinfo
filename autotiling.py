d83e6bdced8da36aa5485c043d65518fca0b106b
piotr <nwg.piotr@gmail.com>
2019-09-23 23:12:20 +0200

    Window measuring fixed; unnecessary type check removed.

--- autotiling.py
+++ autotiling.py
@@ -2,8 +2,9 @@
 # _*_ coding: utf-8 _*_
 
 """
-This script uses the i3ipc python to switch the layout/split for the currently focused window vertical / horizontal,
-depending on the window dimensions. Should work on both sway and i3 window managers.
+This script uses the i3ipc python module to switch the layout/split
+for the currently focused window vertical / horizontal,
+depending on the window dimensions. It works on both sway and i3 window managers.
 
 Inspired by https://github.com/olemartinorg/i3-alternating-layout
 
@@ -23,25 +24,23 @@ i3 = Connection()
 def switch_splitting(i3, e):
     try:
         con = i3.get_tree().find_focused()
-        if not con.type == 'workspace':
-            # con.type == 'floating_con'        - indicates floating enabled in Sway
-            # con.floating                      - may be equal 'auto_on' or 'user_on' in i3
-            is_floating = con.type == 'floating_con' or con.floating and '_on' in con.floating
+        # con.type == 'floating_con'        - indicates floating enabled in Sway
+        # con.floating                      - may be equal 'auto_on' or 'user_on' in i3
+        is_floating = con.type == 'floating_con' or con.floating and '_on' in con.floating
 
-            if not is_floating:
-                new_layout = 'splitv' if con.parent.rect.height > con.parent.rect.width else 'splith'
-                i3.command(new_layout)
+        # Let's exclude floating windows
+        if not is_floating:
+            new_layout = 'splitv' if con.rect.height > con.rect.width else 'splith'
+            i3.command(new_layout)
 
     except Exception as e:
-        print(e)
+        print('Error: {}'.format(e))
         pass
 
 
 def main():
-    # Subscribe to events
     i3.on(Event.WINDOW_FOCUS, switch_splitting)
     i3.on(Event.WINDOW_NEW, switch_splitting)
-
     i3.main()
 
 
