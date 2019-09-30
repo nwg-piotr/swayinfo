28ff5f0342fa956269e9a4f6137d5b4743132182
piotr <nwg.piotr@gmail.com>
2019-09-30 23:34:56 +0200

    warn if battery low

--- au.sh
+++ au.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 # update and refresh the py3status arch-updates module
+
+# Just in case - warn if battery level < threshold
+l=$(acpi | awk -F ',' '{print $2}')
+level=${l:1:-1}
+threshold=40
+if [[ "$level" -lt "$threshold" ]]; then
+	echo "Battery level$l, connect AC!"
+fi
+
 trizen -Syu &&
 echo Press enter to exit; read; py3-cmd refresh arch_updates;
