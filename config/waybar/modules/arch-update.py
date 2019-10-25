40ebf9467a2d032469bb462333c740de113ca9e4
nwg-piotr <nwg.piotr@gmail.com>
2019-10-25 15:59:50 +0200

    Some stuff for waybar

--- config/waybar/modules/arch-update.py
+++ config/waybar/modules/arch-update.py
@@ -4,7 +4,12 @@
 import subprocess
 
 pacman = subprocess.check_output("checkupdates", shell=True).decode("utf-8").splitlines()
-aur = subprocess.check_output("trizen -Qqu -a", shell=True).decode("utf-8").splitlines()
+# trizen returns exit code 1 if no updates found
+aur = []
+try:
+    aur = subprocess.check_output("trizen -Qqu -a", shell=True).decode("utf-8").splitlines()
+except:
+    pass
 
 updates = ''
 if len(pacman) > 0 or len(aur) > 0:
