6b9930cc9dd72e1fdc4f89ea3c65170abf8a599a
piotr <nwg.piotr@gmail.com>
2019-10-26 01:49:56 +0200

    simple scratchpad counter

--- config/waybar/modules/scratchpad.py
+++ config/waybar/modules/scratchpad.py
@@ -7,7 +7,7 @@ i3 = Connection()
 
 con = i3.get_tree()
 
-scratchpad = con.find_named('__i3_scratch') if con.find_named('__i3_scratch') else []
+scratchpad = con.find_named('__i3_scratch')
 leaves = scratchpad[0].floating_nodes
 num_leaves = len(scratchpad[0].floating_nodes)
 output = '{"text":" %s"}' % num_leaves if num_leaves > 0 else '{"text":""}'
