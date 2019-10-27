4b0cfbb9a70513832435c46d4efdecdeea216a7a
piotr <nwg.piotr@gmail.com>
2019-10-27 03:51:34 +0100

    some simplification

--- config/waybar/modules/playerctl.sh
+++ config/waybar/modules/playerctl.sh
@@ -1,24 +1,22 @@
 #!/bin/sh
-# Slightly modified example from
-# https://github.com/Robinhuett/dotfiles/blob/master/.config/waybar/modules/spotify.sh
+# based on https://github.com/Robinhuett/dotfiles/blob/master/.config/waybar/modules/spotify.sh
 
 pname=$(playerctl metadata --format '{{ playerName }}')
 class=$(playerctl metadata --format '{{ lc(status) }}')
 
-if [[ $class == "playing" ]]; then
+if [[ ${class} == "playing" ]] || [[ ${class} == "paused" ]]; then
   info=$(playerctl metadata --format '{{artist}} - {{title}}')
-  if [[ ${#info} > 40 ]]; then
-    info=$(echo $info | cut -c1-40)"..."
+  if [[ ${#info} > 60 ]]; then
+    info=$(echo ${info} | cut -c1-60)"..."
   fi
-  text=$pname": "$info"  "
-elif [[ $class == "paused" ]]; then
-  info=$(playerctl metadata --format '{{artist}} - {{title}}')
-  if [[ ${#info} > 40 ]]; then
-    info=$(echo $info | cut -c1-40)"..."
-  fi
-  text=$pname": "$info"  "
-elif [[ $class == "stopped" ]]; then
-  text=$pname": "
 fi
 
-echo -e "{\"text\":\""$text"\", \"class\":\""$class"\"}"
+if [[ ${class} == "playing" ]]; then
+  text=${pname}": "${info}"  "
+elif [[ ${class} == "paused" ]]; then
+  text=${pname}": "${info}"  "
+elif [[ $class == "stopped" ]]; then
+  text=${pname}": "
+fi
+
+echo -e "{\"text\":\""${text}"\", \"class\":\""${class}"\"}"
