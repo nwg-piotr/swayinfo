bd19b62f3a49c7e6e99f2655b0c2d2927b777c9e
piotr <nwg.piotr@gmail.com>
2019-12-19 02:29:30 +0100

    updated

--- scratchpad_indicator.py
+++ scratchpad_indicator.py
@@ -2,9 +2,17 @@
 # _*_ coding: utf-8 _*_
 
 """
-This code has just been started and is far from any usability at the moment. Come back later!
+This script uses the i3ipc python library to display gtk3 AppIndicator3 tray icon + menu w/ the scratchpad content.
+No AppIndicator3.IndicatorStatus other than ACTIVE has been used here, since it does not (yet?) work on sway:
+it only displays the icon defined with indicator.set_icon_full.
+
+Author: Piotr Miller
+e-mail: nwg.piotr@gmail.com
+Project: https://github.com/nwg-piotr/swayinfo
+License: GPL3
 """
 
+import sys
 import subprocess
 import gi
 
@@ -14,71 +22,119 @@ from gi.repository import Gtk, AppIndicator3, GLib
 
 from i3ipc import Connection
 
-indicator = None
-content_names = []
+indicator = AppIndicator3.Indicator.new('scratchpad_indicator', '', AppIndicator3.IndicatorCategory.APPLICATION_STATUS)
+content_titles = []
+e_menu = None
+
+# edit paths according to your icons location
+ICON_EMPTY: str = '/home/piotr/PycharmProjects/swayinfo/icons/scratchpad_empty.png'
+ICON_SINGLE: str = '/home/piotr/PycharmProjects/swayinfo/icons/scratchpad_single.png'
+ICON_MULTIPLE: str = '/home/piotr/PycharmProjects/swayinfo/icons/scratchpad_multiple.png'
 
 
 def main():
-    i3 = Connection()
-
-    global indicator, sway
+    interval = 1000
     try:
-        result = subprocess.run(['swaymsg', '-t', 'get_seats'], stdout=subprocess.DEVNULL)
-        sway = result.returncode == 0
+        interval = int(sys.argv[1])
+        if interval < 500:
+            interval = 500
     except:
-        sway = False
+        pass
+    
+    connection = Connection()
 
-    check_scratchpad(i3)
-    GLib.timeout_add(3000, check_scratchpad, i3)
+    global indicator, e_menu
+    indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
+    indicator.set_icon_full(ICON_EMPTY, 'Scratchpad')
+
+    e_menu = EmptyMenu()
+    indicator.set_menu(e_menu)
+
+    check_scratchpad(connection)
+    GLib.timeout_add(interval, check_scratchpad, connection)
     Gtk.main()
 
 
-def menu():
+def build_menu():
+    """
+    Build the tray icon menu out of names of windows found in scratchpad
+    """
+    global content_titles
     menu = Gtk.Menu()
-
-    item = Gtk.MenuItem.new_with_label('Update')
-    item.connect('activate', note)
-    menu.append(item)
+    for title in content_titles:
+        item = Gtk.MenuItem.new_with_label(title)
+        item.connect('activate', show_scratchpad, title)
+        menu.append(item)
     
-    item = Gtk.MenuItem.new_with_label('Close notifier')
+    if len(content_titles) > 0:
+        item = Gtk.SeparatorMenuItem()
+        menu.append(item)
+
+    item = Gtk.MenuItem.new_with_label('Close indicator')
     item.connect('activate', quit)
     menu.append(item)
-
     menu.show_all()
+
     return menu
 
 
-def note(_):
-    pass
+class EmptyMenu(Gtk.Menu):
+    """
+    Subclassed to add the 'is_empty' field, which - if found - prevents from setting the menu
+    again and again if scratchpad empty.
+    """
+    def __init__(self):
+        super().__init__()
+        self.is_empty = True
+        item = Gtk.MenuItem.new_with_label('Close indicator')
+        item.connect('activate', quit)
+        self.append(item)
+        self.show_all()
+
+
+def show_scratchpad(item, title):
+    title = title.replace(" ", "\\s")
+    cmd = 'i3-msg [title="^{}*"] scratchpad show'.format(title)
+    subprocess.check_output(cmd, shell=True)
 
 
 def check_scratchpad(connection):
-    global content_names, indicator
-    content_names = []
+    global content_titles, indicator, e_menu
+    current_titles = []
     
     scratchpad = connection.get_tree().find_named('__i3_scratch')
     leaves = scratchpad[0].floating_nodes
     
     for node in leaves:
-        content_names.append(node.name)
-    
-    if len(content_names) > 0:
-        if not indicator:
-            indicator = AppIndicator3.Indicator.new('scratchpad_indicator', '',
-                                                    AppIndicator3.IndicatorCategory.APPLICATION_STATUS)
-        if len(content_names) == 1:
-            indicator.set_icon_full('/home/piotr/PycharmProjects/swayinfo/icons/scratchpad.png', 'Scratchpad')
+        # find names on sway
+        if node.name:
+            current_titles.append(node.name)
+        # find names on i3
         else:
-            indicator.set_icon_full('/home/piotr/PycharmProjects/swayinfo/icons/scratchpad2.png', 'Scratchpad')
-        indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
-        indicator.set_menu(menu())
-        
-    else:
-        if indicator:
-            pass
-        # todo hide indicator in some way here! Setting status PASSIVE does not work on sway.
+            current_titles.append(node.nodes[0].name)
     
-    print(content_names)
+    if len(current_titles) > 0:
+
+        if len(current_titles) == 1:
+            indicator.set_icon_full(ICON_SINGLE, 'Scratchpad')
+        else:
+            indicator.set_icon_full(ICON_MULTIPLE, 'Scratchpad')
+
+        if not current_titles == content_titles:
+            content_titles = current_titles
+            indicator.set_menu(build_menu())
+    else:
+        indicator.set_icon_full(ICON_EMPTY, 'Scratchpad')
+        if indicator.get_menu():
+            try:
+                indicator.get_menu().is_empty
+            except Exception as e:
+                indicator.set_menu(e_menu)
+
+        if len(content_titles) > 0:
+            indicator.set_menu(build_menu())
+            content_titles = []
+
     return True
 
 
