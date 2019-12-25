af2188f2af243f4105426a4b15cff13c9fd087df
piotr <nwg.piotr@gmail.com>
2019-12-25 23:06:30 +0100

    Acquire a date with `zenity --calendar` and open it in calendar.google.com with Chromium

--- calendar.py
+++ calendar.py
@@ -2,7 +2,7 @@
 # _*_ coding: utf-8 _*_
 
 """
-This script acquires a date with `zenity --calendar` and opens it in calendar.google.com with Chromium
+This script acquires a date with `zenity --calendar` and opens it with calendar.google.com in Chromium
 """
 
 import subprocess
@@ -10,9 +10,8 @@ import subprocess
 try:
     date = subprocess.check_output('zenity --calendar', shell=True).decode("utf-8")
     d_m_y = date.split('.')
-    day, month, year = d_m_y[0], d_m_y[1], d_m_y[2]
-
-    command = 'chromium https://calendar.google.com/calendar/r/month/{}/{}/{}'.format(year, month, day)
+    day, month, year = d_m_y[0].strip(), d_m_y[1].strip(), d_m_y[2].strip()
+    command = 'chromium https://calendar.google.com/calendar/r/day/{}/{}/{}'.format(year, month, day)
     subprocess.Popen(command, shell=True)
 except subprocess.CalledProcessError:
     pass
