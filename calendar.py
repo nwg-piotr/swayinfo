0761910a2b6be17b184eaccc65f505d02fce4603
piotr <nwg.piotr@gmail.com>
2019-12-26 11:21:37 +0100

    month view instead of the day

--- calendar.py
+++ calendar.py
@@ -11,7 +11,7 @@ try:
     date = subprocess.check_output('zenity --calendar', shell=True).decode("utf-8")
     d_m_y = date.split('.')
     day, month, year = d_m_y[0].strip(), d_m_y[1].strip(), d_m_y[2].strip()
-    command = 'chromium https://calendar.google.com/calendar/r/day/{}/{}/{}'.format(year, month, day)
+    command = 'chromium https://calendar.google.com/calendar/r/month/{}/{}/{}'.format(year, month, day)
     subprocess.Popen(command, shell=True)
 except subprocess.CalledProcessError:
     pass
